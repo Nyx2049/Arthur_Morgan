49997db4d2a58f3c14e32a6e807b39dbafc1397c
Nyx2049 <86938755+Nyx2049@users.noreply.github.com>
2022-06-23 17:09:28 +0800

    Update Convex_Hull.cpp
    完善凸包算法第一版，原本凸包算法，主要的问题在于判等函数上，没有考虑到点在线上时的情况，应该采用枚举的方式单独考虑

--- 凸包算法/Convex_Hull.cpp
+++ 凸包算法/Convex_Hull.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 typedef struct std::pair<double, double> Point;
+enum Ternary
+{
+    Yin = -1,
+    Yang = 1,
+    Yuan = 0,
+};
 //测试pair的用法。
 void test01()
 {
@@ -9,6 +15,7 @@ void test01()
     std::cout << p.second << std::endl;
 }
 
+//判等函数
 double getAver(std::vector<bool> &_list)
 {
     double sum;
@@ -19,6 +26,52 @@ double getAver(std::vector<bool> &_list)
     return sum / _list.size();
 }
 
+//这是三元的情况。
+double getAver_2_0(std::vector<int> &_list)
+{
+    double sum;
+    for (int a = 0; a < _list.size(); a++)
+    {
+        if (_list[a] == Yuan)
+        {
+            if (a == 0)
+            {
+                _list[a] = _list[a + 1];
+            }
+            _list[a] = _list[a - 1];
+        }
+        sum += _list[a];
+    }
+    return sum / _list.size();
+}
+// 经过自己测试，第二版本 依然不正确，如果上一一个值，也是Yuan,那么无法正确,下为第三版本。
+double getAver_3_0(std::vector<int> &_list)
+{
+    double sum;
+    int v = 0;
+    for (auto &&_v : _list)
+    {
+        if (_v != 0)
+        {
+            v = _v;
+            break;
+        }
+    }
+    if (v == 0)
+    {
+        return 1.0;
+    }
+    for (int a = 0; a < _list.size(); a++)
+    {
+        if (_list[a] == 0)
+        {
+            _list[a] = v;
+        }
+        sum += _list[a];
+    }
+    return sum / _list.size();
+}
+//根据两个点坐标湖获取对应的k，b值。
 bool getKandB(Point &A, Point &B, double &k, double &b)
 {
     if (A.first == B.first && A.second == B.second)
@@ -64,27 +117,61 @@ bool PointEquation_1_0(double &k, double &b, Point &p)
     }
 }
 
+// 2.0版本考虑到，点数据正好在线上的功能，选择采用枚举+二次判断的方式来作解法，同时需要一个新的justice_2.0来作判断
+Ternary PointEquation_2_0(double &k, double &b, Point &p)
+{
+    if (k == NULL)
+    {
+        if (p.first < b)
+            return Yang;
+        else if (p.first > b)
+            return Yin;
+        return Yuan;
+    }
+    else if (b == NULL)
+    {
+        if (p.second < k)
+            return Yang;
+        else if (p.second > k)
+            return Yin;
+        return Yuan;
+    }
+    else
+    {
+        if (p.second - k * p.first - b < 0)
+            return Yang;
+        else if (p.second - k * p.first - b > 0)
+            return Yin;
+        return Yuan;
+    }
+}
+
 void getConvexHull_1_0(std::vector<Point> &pts, std::vector<Point> &edgePts)
 {
-    bool rightEdge = false;
+    //双循环遍历所有的点与点连线的可能
     for (int num = 0; num < pts.size(); num++)
     {
+        bool rightEdge = false;
         for (int _num = 0; _num < pts.size(); _num++)
         {
-            std::vector<bool> justice;
+            if (pts[num].first == 0.25)
+            {
+                int aksldja = 0;
+            }
+            std::vector<int> justice;
             double k, b;
             if (!getKandB(pts[num], pts[_num], k, b))
                 continue;
             for (auto &&pt : pts)
             {
-                justice.push_back(PointEquation_1_0(k, b, pt));
+                justice.push_back(PointEquation_2_0(k, b, pt));
             }
-            double value = getAver(justice);
+            double value = getAver_3_0(justice);
             std::cout << "value:" << value << std::endl;
-            if ((value == 0) || (value == 1))
+            if ((value == -1) || (value == 1))
             // if (getAver(justice) == 0|| (getAver(justice) == 1)) //为什么结果不同 和上一句
             {
-                //std::cout << "getAver(justice):" << getAver(justice) << std::endl;
+                // std::cout << "getAver(justice):" << getAver(justice) << std::endl;
                 rightEdge = true;
                 break;
             }
@@ -92,6 +179,7 @@ void getConvexHull_1_0(std::vector<Point> &pts, std::vector<Point> &edgePts)
         std::cout << "rightEdge: " << rightEdge << std::endl;
         if (rightEdge)
         {
+            std::cout << "pts[num]:" << pts[num].first << ", " << pts[num].second << std::endl;
             edgePts.push_back(pts[num]);
         }
     }
@@ -99,17 +187,19 @@ void getConvexHull_1_0(std::vector<Point> &pts, std::vector<Point> &edgePts)
 
 void test02()
 {
+    Point p3 = std::make_pair(0.25, 0.5);
     Point p1 = std::make_pair(0.0, 0.0);
     Point p2 = std::make_pair(1.0, 1.0);
-    Point p3 = std::make_pair(0.25, 0.5);
     Point p4 = std::make_pair(0.0, 1.0);
     Point p5 = std::make_pair(1.0, 0.0);
+    Point p6 = std::make_pair(2.0, 2.0);
+    Point p7 = std::make_pair(0.0, 4.0);
+    Point p8 = std::make_pair(4.0, 0.0);
+    Point p9 = std::make_pair(0.1, 0.5);
+    Point p10 = std::make_pair(0.5, 0.5);
+
     std::vector<Point> pts, edgePts;
-    pts.push_back(p1);
-    pts.push_back(p2);
-    pts.push_back(p3);
-    pts.push_back(p4);
-    pts.push_back(p5);
+    pts = {p1, p2, p3, p4, p5, p5, p6, p7, p8, p1, p7, p8, p9, p10};
     getConvexHull_1_0(pts, edgePts);
     std::cout << "edgePts.size():" << edgePts.size() << std::endl;
     int a = 1;
@@ -121,18 +211,46 @@ void test02()
     }
 }
 
+void test04()
+{
+    Point p1 = std::make_pair(1.0, 0.1);
+    Point p2 = std::make_pair(0.0, 0);
+    Point p3 = std::make_pair(100.0, 0);
+    Point p4 = std::make_pair(1.0, 0.0);
+    Point p5 = std::make_pair(40.0, 0);
+    Point p6 = std::make_pair(27.0, 0);
+    Point p7 = std::make_pair(30.0, 0);
+    Point p8 = std::make_pair(50, 100);
+
+    std::vector<Point> pts, edgePts;
+    pts = {p1, p2, p3, p4, p5, p5, p6, p7, p8};
+    getConvexHull_1_0(pts, edgePts);
+    std::cout << "edgePts.size():" << edgePts.size() << std::endl;
+    int a = 1;
+    for (auto &&it : edgePts)
+    {
+        char b = a + '0';
+        std::cout << "edgePts[" << b << "]:" << it.first << ", " << it.second << std::endl;
+        a++;
+    }
+}
 void test_getAver()
 {
-    std::vector<bool> vb;
-    vb.push_back(true);
-    vb.push_back(true);
-    vb.push_back(true);
-    vb.push_back(true);
-    vb.push_back(false);
-    vb.push_back(true);
-    vb.push_back(true);
-    vb.push_back(true);
-    std::cout << " getAver(vb):" << getAver(vb) << std::endl;
+    //使用枚举变量的心得， 整数型的枚举可以自动转换成整型，但是整形不能直接当作枚举
+    std::vector<int> vb;
+    vb.push_back(0);
+    vb.push_back(0);
+    vb.push_back(0);
+    vb.push_back(0);
+    vb.push_back(0);
+    vb.push_back(0);
+    // vb.push_back(1);
+    // vb.push_back(1);
+    // vb.push_back(0);
+    // vb.push_back(Yang);
+    vb.push_back(Yuan);
+    double c = getAver_3_0(vb);
+    std::cout << " getAver(vb):" << c << std::endl;
 }
 
 // bool PointEquation_2_0(std::vector<Point> &vP, const double &k, const double &b)
@@ -153,8 +271,13 @@ void test_PointEquation_1_0()
 }
 int main()
 {
-    test02();
+    test04();
     // test_getAver();
-    //test_PointEquation_1_0(); //经过测试，没有问题，同一方向的返回值一致
+    // test_PointEquation_1_0(); //经过测试，没有问题，同一方向的返回值一致
     return 0;
 }
+
+/*study：
+一、使用枚举变量的心得， 整数型的枚举可以自动转换成整型，但是整形不能直接当作枚举
+
+*/
