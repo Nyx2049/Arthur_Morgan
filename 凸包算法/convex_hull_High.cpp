2c83dbdadffa9142d02849fd6962668d60217eb2
ljm_mac <lijinmengde@163.com>
2022-06-24 00:48:12 +0800

    插件的更新

--- 凸包算法/convex_hull_High.cpp
+++ 凸包算法/convex_hull_High.cpp
@@ -1,1 +1,8 @@
-//更高级的凸包计算方式
+/*
+ * @Author: ljm_mac lijinmengde@163.com
+ * @Date: 2022-06-24 00:17:28
+ * @LastEditors: ljm_mac lijinmengde@163.com
+ * @LastEditTime: 2022-06-24 00:46:15
+ * @FilePath: /Arthur_Morgan/凸包算法/convex_hull_High.cpp
+ * @Description: 这是一个更高级的凸包算法
+ */
