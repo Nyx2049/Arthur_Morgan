6e505872ecb47a6ac17f068a75aa72f8d39b0a2d
ljm_mac <lijinmengde@163.com>
2022-06-27 00:10:50 +0800

    本次带有配置CGAL的一些配置文件

--- 凸包算法/convex_hull_High.cpp
+++ 凸包算法/convex_hull_High.cpp
@@ -2,7 +2,7 @@
  * @Author: ljm_mac lijinmengde@163.com
  * @Date: 2022-06-24 00:17:28
  * @LastEditors: ljm_mac lijinmengde@163.com
- * @LastEditTime: 2022-06-24 00:46:15
+ * @LastEditTime: 2022-06-27 00:07:34
  * @FilePath: /Arthur_Morgan/凸包算法/convex_hull_High.cpp
- * @Description: 这是一个更高级的凸包算法
+ * @Description: 这是一个更优秀的凸包算法，方法来自于计算几何这本书
  */
